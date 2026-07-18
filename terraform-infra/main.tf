d7c2aa3195139d8bef3e988e486483af83d54b7b
abdulkhan <khanabdulrahim378@gmail.com>
2026-07-18 20:10:28 +0530

    main.tf with string interpolation

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "ntms-rg"
+  name     = "${var.environment}-${var.business_unit}-rg"
   location = var.location
   tags = {
     environment = var.environment
@@ -8,21 +8,21 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "ntms-vnet"
+  name                = "${var.environment}-${var.business_unit}-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name
+  name                 = "${var.environment}-${var.business_unit}-${var.subnet_name}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "ntms-nsg"
+  name                = "${var.environment}-${var.business_unit}-nsg"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
