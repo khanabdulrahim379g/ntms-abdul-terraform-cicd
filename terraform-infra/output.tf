d1a4b8f9876db9c707a871d77185e5fd9918bcef
abdulkhan <khanabdulrahim378@gmail.com>
2026-07-20 00:28:26 +0530

    plan.yml updated

--- terraform-infra/output.tf
+++ terraform-infra/output.tf
@@ -3,25 +3,18 @@ output "rgname" {
 
 }
 
-output "rgid" {
-  value = azurerm_resource_group.rg.id
-}
+
 
 output "vnetname" {
   value = azurerm_virtual_network.vnet.name
 }
 
-output "vnetid" {
-  value = azurerm_virtual_network.vnet.id
 
+output "subnet_name" {
+  value = azurerm_subnet.subnet.name
 }
 
-output "subnetid" {
-  value = azurerm_subnet.subnet.id
-}
 
-output "nsgid" {
-  value = azurerm_network_security_group.nsg.id
-}
+
 
 
